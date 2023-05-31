e67d52d07f279abdf6277e558940f5a579a1b1f9
Ming52 <135110444+Ming52@users.noreply.github.com>
2023-05-31 22:21:59 +0800

    Add files via upload

---
+++ walle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+
+# In[ ]:
+
+
+import os
+
+
+# In[ ]:
+
+
+from fastai.vision.all import *
+
+
+# In[ ]:
+
+
+import pathlib
+
+
+# In[ ]:
+
+
+temp = pathlib.PosixPath
+
+
+# In[ ]:
+
+
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+# In[ ]:
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+# In[ ]:
+
+
+model_path = os.path.join(path, 'export.pkl')
+
+
+# In[ ]:
+
+
+learn_inf = load_learner(model_path)
+
+
+# In[ ]:
+
+
+pathlib.PosixPath = temp
+
+
+# In[ ]:
+
+
+uploaded_file = st.file_uploader("Choose an image...",
+                                type=["jpg","png","jpeg"])
+
+
+# In[ ]:
+
+
+if uploaded_file is not None:
+    img = PILImage.create(uploaded_file)
+    st.image(img.to_thumb(500,500),caption='Your Image')
+    pred,pred_idx,probs = learn_inf.predict(img)
+    st.write(f'Prediction:{pred};Probability:{probs[pred_idx]:.04f}')
+
