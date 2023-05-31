2b1570b055fa810c18d02c64fe0b96a8d1548fcc
Ming52 <135110444+Ming52@users.noreply.github.com>
2023-05-31 22:44:29 +0800

    Add files via upload

--- walle.py
+++ walle.py
@@ -1,79 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import streamlit as st
-
-
-# In[ ]:
-
-
 import os
-
-
-# In[ ]:
-
-
 from fastai.vision.all import *
 
-
-# In[ ]:
-
-
-import pathlib
-
-
-# In[ ]:
-
-
-temp = pathlib.PosixPath
-
-
-# In[ ]:
-
-
-pathlib.PosixPath = pathlib.WindowsPath
-
-
-# In[ ]:
-
-
 path = os.path.dirname(os.path.abspath(__file__))
-
-
-# In[ ]:
-
-
-model_path = os.path.join(path, 'export.pkl')
-
-
-# In[ ]:
-
+model_path = os.path.join(path, "export.pkl")
 
 learn_inf = load_learner(model_path)
 
+st.title("Image Classification App")
+st.write("Upload an image and the app will predict the corresponding label.")
 
-# In[ ]:
-
-
-pathlib.PosixPath = temp
-
-
-# In[ ]:
-
-
-uploaded_file = st.file_uploader("Choose an image...",
-                                type=["jpg","png","jpeg"])
-
-
-# In[ ]:
-
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img = PILImage.create(uploaded_file)
-    st.image(img.to_thumb(500,500),caption='Your Image')
-    pred,pred_idx,probs = learn_inf.predict(img)
-    st.write(f'Prediction:{pred};Probability:{probs[pred_idx]:.04f}')
+    image = PILImage.create(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
+    pred, pred_idx, probs = learn_inf.predict(image)
+    st.write(f"Prediction: {pred}; Probability: {probs[pred_idx]:.04f}")
